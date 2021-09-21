43635300ab1e80169e1eac09f020e2431dcf3c44
Cherish0207 <cheshire_1207@foxmail.com>
2021-09-21 20:41:18 +0800

    2.优化1.添加有序标记：一些情况可以提前结束循环

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,15 +1,19 @@
 function bubbleSort (arr) {
   for (let i = 0; i < arr.length -1; i++){
     console.log(i);
+    let isSorted = true
     for (let j = 0; j < arr.length - i - 1; j++){
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j + 1]
         arr[j + 1] = arr[j]
         arr[j] = temp
+       isSorted = false
       }
     }
+    if(isSorted) break
   }
   return arr
 }
-console.log(bubbleSort([5, 8, 6, 3, 9, 2, 1, 7]));
-// 原始实现：大循环N-1次
+// console.log(bubbleSort([5, 8, 6, 3, 9, 1, 2, 7]));
+// 原始实现：大循环N-1次 即7次
+// 优化1.添加有序标记：一些情况可以提前结束循环 6次
